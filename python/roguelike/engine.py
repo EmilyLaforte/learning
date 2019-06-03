a0c3452baeaefdf099b09c96f0b6308cbfffa403
Emily Laforte <emily.hinata.laforte@gmail.com>
2019-06-02 20:04:35 -0400

    Finish lesson 2

--- python/roguelike/engine.py
+++ python/roguelike/engine.py
@@ -1,30 +1,41 @@
 import tcod as libtcod
 
-from entity import Entity
+from .entity import Entity
 
-from input_handlers import handle_keys
+from .input_handlers import handle_keys
 
-from render_functions import clear_all, render_all
+from .render_functions import clear_all, render_all
+
+from .map_objects.game_map import GameMap
 
 def main():
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
+
+    colors = {
+        "dark wall" : libtcod.Color(0, 0, 100),
+        "dark ground" : libtcod.Color(50, 50, 150)
+    }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@" , libtcod.turquoise)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "&" , libtcod.blue)
     entities = [npc, player]
-    libtcod.console_set_custom_font("python/roguelike/arial10x10.png", libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    libtcod.console_set_custom_font("roguelike/arial10x10.png", libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
     libtcod.console_init_root(screen_width, screen_height, "tr**hy rogue like", False)
 
     con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
-        render_all(con, entities, screen_width, screen_height)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
 
         clear_all(con, entities)
@@ -37,7 +48,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if exit:
             return True
